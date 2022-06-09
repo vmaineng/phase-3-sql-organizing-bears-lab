a49956b94d7c19ee9edc45fa7191616bfd8f0052
Mai Vang <vmaineng@gmail.com>
2022-06-08 19:05:37 -0700

    commit tree fa8925fa360cb7cc87ab4d70089a3a6c699330cc

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES ('Mr.Chocolate', 2, 'F', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Rowdy', 2, 'M', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Tabitha', 2, 'F', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Sergeant Brown', 2, 'F', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Melissa', 2, 'F', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Grinch', 2, 'M', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES ('Wendy', 2, 'M', 'Yellow', 'Happy', 'Alive');
+INSERT INTO bears VALUES (null, 2, 'M', 'Yellow', 'Happy', 'Alive');
+
+
